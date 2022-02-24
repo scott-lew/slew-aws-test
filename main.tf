b28e1987fb9e8f0a89afa4d11eb58da67161cd4c
Scott Lew <slew@cloudops.com>
2022-02-24 11:09:23 -0500

    create s3

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "~> 1.1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.1.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "aws-test" {
+  bucket = "slew-aws-test-111"
+}
